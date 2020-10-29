a6b25dad1c63d6fd7deb8d5dda21afe0d015134b
syamkerai <syamkerai@hotmail.com>
2020-10-29 13:10:30 +1100

    p q r s t anotated correctly
    p q r s t anotated correctly
    and labled accordingly

--- part2.m
+++ part2.m
@@ -1,6 +1,6 @@
 clear all
 clc
-load Sample_1.mat;
+load Sample_6.mat;
 rawData=Orig_Sig;
 numSamples = length(rawData);
 maxValue = max(rawData);
@@ -10,26 +10,65 @@ limBuffer = 50;
 maxYLim = maxValue + limBuffer;
 minYLim = minValue - limBuffer;
 % only consider peaks above the 70th percentile
-peakThresholdPct = 0.65;
+peakThresholdPct = 0.53;
 peakThreshold = minValue + ((maxValue - minValue) * peakThresholdPct);
-% increase window for more smoothing
-movMeanWindow = 10;
 
 
-% use movmean to clean up spikes
-d = designfilt('lowpassiir', 'FilterOrder', 2, 'HalfPowerFrequency' ,0.15, 'DesignMethod','butter');
+
+% use filtfilt to clean up spikes
+d = designfilt('lowpassiir', 'FilterOrder', 2, 'HalfPowerFrequency' ,0.08, 'DesignMethod','butter');
 filteredData = filtfilt(d, rawData);
 % filteredData = movmean(rawData, movMeanWindow); % 10 is moving window
 peakLocs = find(islocalmax(filteredData) & filteredData > peakThreshold);
-dipLocs = find(islocalmin(filteredData));
+valleyLocs = find(islocalmin(filteredData, 'MinProminence', 3));
 
-hold on 
+P_locs = [];
+Q_locs = [];
+R_locs = peakLocs;
+S_locs = [];
+T_locs = [];
+
+% loop through valley locations near peaks to assign P,Q,S,T
+peakIdx = 1;
+for valleyIdx = 2:length(valleyLocs)-2
+    P_loc = valleyLocs(valleyIdx - 1);
+    Q_loc = valleyLocs(valleyIdx);
+    R_loc = peakLocs(peakIdx);
+    S_loc = valleyLocs(valleyIdx + 1);
+    T_loc = valleyLocs(valleyIdx + 2);
+    
+    if Q_loc < R_loc && R_loc < S_loc && peakIdx < length(peakLocs)
+        peakIdx = peakIdx + 1;
+        % add to locs array
+        P_locs = [P_locs P_loc];
+        Q_locs = [Q_locs Q_loc];
+        S_locs = [S_locs S_loc];
+        T_locs = [T_locs T_loc];
+    end
+end
+
+hold on;
+plot(P_locs, filteredData(P_locs), 'bo', 'MarkerSize', 10);
+plot(Q_locs, filteredData(Q_locs), 'rs', 'MarkerSize', 10);
+plot(R_locs, filteredData(R_locs), 'rv', 'MarkerSize', 10);
+plot(S_locs, filteredData(S_locs), 'kx', 'MarkerSize', 10);
+plot(T_locs, filteredData(T_locs), 'md', 'MarkerSize', 10);
+
+%plot(valleyLocs, filteredData(valleyLocs), 'b^');
 plot(rawData, 'g--');
-plot([0, numSamples],[peakThreshold, peakThreshold], 'r:');
 plot(filteredData, 'b');
-plot(peakLocs, filteredData(peakLocs), 'rv');
-plot(dipLocs, filteredData(dipLocs), 'b^');
-hold off
+plot([0, numSamples],[peakThreshold, peakThreshold], 'r:');
+hold off;
+legend('P', 'Q', 'R', 'S', 'T', 'ECG Raw signal', 'Filtered Data', 'Threshold line')
 title('Analyzed ECG')
 axis([0 numSamples minYLim maxYLim])
 
+
+% heart rate calculation and display
+numPeaks = length(peakLocs);
+heartRate=(numPeaks * 60) / 10.0; % 60s in a min, 10s of samples
+
+PR_val = 0;
+QT_val = 0;
+QRS_dur = 0;
+fprintf('Heart rate analysis\nHeart rate: %0.2f[b/m]\nP-R interval: %0.2f[s]\nQ-T interval: %0.2f[s]\nQRS duration: %0.2f[s]\n', heartRate, PR_val, QT_val, QRS_dur);
