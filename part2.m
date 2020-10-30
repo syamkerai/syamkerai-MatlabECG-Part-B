bda935f45dfdb655735d1a7597d731117901b80c
syamkerai <syamkerai@hotmail.com>
2020-10-30 15:36:48 +1100

    final working

--- part2.m
+++ part2.m
@@ -1,6 +1,6 @@
 clear all
 clc
-load Sample_4.mat;
+load Sample_8.mat;
 rawData=Orig_Sig;
 numSamplesPerSec = 360;
 numSamples = length(rawData);
@@ -9,30 +9,29 @@ minValue = min(rawData);
 limBuffer = 50;
 maxYLim = maxValue + limBuffer;
 minYLim = minValue - limBuffer;
-% only consider peaks above peakThresholdPct
-peakThresholdPct = 0.5;
-peakThreshold = minValue + ((maxValue - minValue) * peakThresholdPct);
-set(gca,'XTick', [numSamplesPerSec:numSamplesPerSec:numSamples]);
-set(gca,'XTickLabel', [1:10]);
+
 % use filtfilt to clean up spikes
 d = designfilt('lowpassiir', 'FilterOrder', 2, 'HalfPowerFrequency' ,0.08, 'DesignMethod','butter');
 filteredData = filtfilt(d, rawData);
-peakLocs = find(islocalmax(filteredData) & filteredData > peakThreshold);
-valleyLocs = find(islocalmin(filteredData, 'MinProminence', 2));
+peakLocs = find(islocalmax(filteredData));
+valleyLocs = find(islocalmin(filteredData));
+valleyLocs = [1;valleyLocs];
 
 P_locs = [];
 Q_locs = [];
-R_locs = peakLocs;
+R_locs = [];
 S_locs = [];
 T_locs = [];
 
 % loop through valley locations near peaks to assign P,Q,S,T
+peakValleyThreshold = (max(filteredData) - min(filteredData)) * 0.6;
 peakIdx = 1;
 
 for valleyIdx = 1:length(valleyLocs)-1
     if peakIdx > length(peakLocs)
         break
     end
+    
     % sometimes P_loc doesn't exist since there isn't a dip 
     % assume the first value
     P_loc = 1;
@@ -43,6 +42,12 @@ for valleyIdx = 1:length(valleyLocs)-1
     % assume the the value
     T_loc = numSamples;
     
+    % only consider a peak if valley-peak-valley is above a threshold
+    if (filteredData(R_loc) * 2 - filteredData(Q_loc) - filteredData(S_loc)) < peakValleyThreshold
+        peakIdx = peakIdx + 1;
+        continue;
+    end
+    
     if valleyIdx > 1
         P_loc = valleyLocs(valleyIdx - 1);
     end
@@ -51,11 +56,13 @@ for valleyIdx = 1:length(valleyLocs)-1
         T_loc = valleyLocs(valleyIdx + 2);
     end
     
+    % if Q R S is valley-peak-valley
     if Q_loc < R_loc && R_loc < S_loc
         peakIdx = peakIdx + 1;
         % add to locs array
         P_locs = [P_locs;P_loc];
         Q_locs = [Q_locs;Q_loc];
+        R_locs = [R_locs;R_loc];
         S_locs = [S_locs;S_loc];
         T_locs = [T_locs;T_loc];
     end
@@ -70,17 +77,18 @@ plot(T_locs, filteredData(T_locs), 'md', 'MarkerSize', 10);
 
 plot(rawData, 'g--');
 plot(filteredData, 'b');
-plot([0, numSamples],[peakThreshold, peakThreshold], 'r:');
 % plot(valleyLocs, filteredData(valleyLocs), 'b^');
 
 hold off;
-legend('P', 'Q', 'R', 'S', 'T', 'ECG Raw signal', 'Filtered Data', 'Threshold line ')
-title('Analyzed ECG')
-axis([0 numSamples minYLim maxYLim])
+set(gca,'XTick', [numSamplesPerSec:numSamplesPerSec:numSamples]);
+set(gca,'XTickLabel', [1:10]);
+legend('P', 'Q', 'R', 'S', 'T', 'ECG Raw Data', 'Filtered Data')
+title('Analyzed ECG');
+axis([0 numSamples minYLim maxYLim]);
 
 
 % heart rate calculation and display
-numPeaks = length(peakLocs);
+numPeaks = length(R_locs);
 heartRate=(numPeaks * 60) / 10.0; % 60s in a min, 10s of samples
 PR_dur = mean(R_locs - P_locs) / numSamplesPerSec;
 QT_dur = mean(T_locs - Q_locs) / numSamplesPerSec;
