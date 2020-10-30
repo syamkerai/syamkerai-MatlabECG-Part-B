7df1e0da5314e5d673120ceb3cefb9e36441801a
syamkerai <syamkerai@hotmail.com>
2020-10-30 14:21:46 +1100

    thresholds

--- part2.m
+++ part2.m
@@ -1,7 +1,8 @@
 clear all
 clc
-load Sample_1.mat;
+load Sample_4.mat;
 rawData=Orig_Sig;
+numSamplesPerSec = 360;
 numSamples = length(rawData);
 maxValue = max(rawData);
 minValue = min(rawData);
@@ -9,9 +10,10 @@ limBuffer = 50;
 maxYLim = maxValue + limBuffer;
 minYLim = minValue - limBuffer;
 % only consider peaks above peakThresholdPct
-peakThresholdPct = 0.50;
+peakThresholdPct = 0.5;
 peakThreshold = minValue + ((maxValue - minValue) * peakThresholdPct);
-
+set(gca,'XTick', [numSamplesPerSec:numSamplesPerSec:numSamples]);
+set(gca,'XTickLabel', [1:10]);
 % use filtfilt to clean up spikes
 d = designfilt('lowpassiir', 'FilterOrder', 2, 'HalfPowerFrequency' ,0.08, 'DesignMethod','butter');
 filteredData = filtfilt(d, rawData);
@@ -52,10 +54,10 @@ for valleyIdx = 1:length(valleyLocs)-1
     if Q_loc < R_loc && R_loc < S_loc
         peakIdx = peakIdx + 1;
         % add to locs array
-        P_locs = [P_locs P_loc];
-        Q_locs = [Q_locs Q_loc];
-        S_locs = [S_locs S_loc];
-        T_locs = [T_locs T_loc];
+        P_locs = [P_locs;P_loc];
+        Q_locs = [Q_locs;Q_loc];
+        S_locs = [S_locs;S_loc];
+        T_locs = [T_locs;T_loc];
     end
 end
 
@@ -80,8 +82,19 @@ axis([0 numSamples minYLim maxYLim])
 % heart rate calculation and display
 numPeaks = length(peakLocs);
 heartRate=(numPeaks * 60) / 10.0; % 60s in a min, 10s of samples
+PR_dur = mean(R_locs - P_locs) / numSamplesPerSec;
+QT_dur = mean(T_locs - Q_locs) / numSamplesPerSec;
+QRS_dur = mean(S_locs - Q_locs) / numSamplesPerSec;
 
-PR_val = 0;
-QT_val = 0;
-QRS_dur = 0;
-fprintf('Heart rate analysis\nHeart rate: %0.2f[b/m]\nP-R interval: %0.2f[s]\nQ-T interval: %0.2f[s]\nQRS duration: %0.2f[s]\n', heartRate, PR_val, QT_val, QRS_dur);
+fprintf('Heart rate analysis\nHeart rate: %0.2f[b/m]\nP-R interval: %0.2f[s]\nQ-T interval: %0.2f[s]\nQRS interval: %0.2f[s]\n', heartRate, PR_dur, QT_dur, QRS_dur);
+
+if PR_dur < 0.12 || PR_dur > 0.20
+    fprintf('Your P-R interval is abnormal (0.12 to 0.20[s])\n')
+end
+if QT_dur >= 0.38
+    fprintf('Your Q-T interval is abnormal (>= 0.38[s])\n')
+end
+if QRS_dur>0.1
+    fprintf('Your QRS interval is abnormal (>= 0.1[s])\n')
+end
+
